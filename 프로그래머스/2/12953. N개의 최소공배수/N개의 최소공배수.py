22c60d49558d3a2028ac7fb755976edf1dc07b1d
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-03 16:50:53 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,16 +1,18 @@
-def solution(arr):
-    answer=arr[0]
-    
-    def gcd(a, b) :
-        while(b!=0) :
+def solution(arr) :
+    answer = 0
+    def mdf(a,b) :
+        while b != 0 :
             temp = b
             b = a%b
             a = temp
         return a
     
-    for i in range(1, len(arr)) :
-        b = arr[i]
-        
-        answer*=b/gcd(b,answer)
+    for i in range(len(arr)-1) :
+        if i == 0 :
+            mid = arr[i]*arr[i+1] // mdf(arr[i], arr[i+1])
+            answer=mid
+        else :
+            mid = answer*arr[i+1] // mdf(answer,arr[i+1])
+            answer=mid
     
     return answer
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
 
 ### 성능 요약
 
-메모리: 9.24 MB, 시간: 0.02 ms
+메모리: 9.32 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 23일 00:24:46
+2026년 03월 03일 16:50:51
 
 ### 문제 설명
 
