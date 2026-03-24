1aad87bea8c0fa3201342a236de48ec2f689cf7d
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-24 23:53:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 158.00 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 134.10 ms
+메모리: 13.8 MB, 시간: 158.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 25일 14:05:12
+2026년 03월 24일 23:53:27
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,16 +1,19 @@
 def solution(s):
     answer = -1
     stack = []
+    length = len(s)
     
-    for ch in s :
-        if stack and ch == stack[-1] :
+    for i in range(length) :
+        if not stack :
+            stack.append(s[i])
+        elif stack[-1] == s[i] :
             stack.pop()
         else :
-            stack.append(ch)
+            stack.append(s[i])
     
     if stack :
         return 0
     else :
         return 1
-    
+
     return answer
