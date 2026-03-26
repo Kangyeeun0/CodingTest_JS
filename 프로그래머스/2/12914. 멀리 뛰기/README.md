77066777063bdae27fb1db812e09c1beaafb947a
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-26 16:41:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.44 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 9.46 MB, 시간: 0.75 ms
+메모리: 9.14 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 04일 13:53:45
+2026년 03월 26일 16:40:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,16 +1,11 @@
 def solution(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-
-    # dp[i] : i 계단까지 오르는 방법의 수
-    dp = [0] * (n + 1)
-    dp[0] = 1 
-    dp[1] = 1  
-
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]  # 1칸 전 + 2칸 전 경우 합
-
-    return dp[n] %1234567
-
+    answer = 0
+    dp = [0] * (n+1)
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i in range(n-1) :
+        dp[i+2] = dp[i+1] + dp[i]
+    # print(dp)
+    answer = dp[-1]
+    return answer % 1234567
