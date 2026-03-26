cc6c2a14343f6d9483073ffe5bc81a55aaac771c
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-26 16:28:34 +0900

    [level 2] Title: 구명보트, Time: 13.42 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,14 @@
 def solution(people, limit):
+    answer = 0
     people.sort()
     
     print(people)
-    
     left = 0
     right = len(people) - 1
-    answer = 0
     
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1  # 같이 탐
-        right -= 1    # 무거운 사람은 항상 탐
-        answer += 1
-    
+    while left <= right :
+        if people[left] + people[right] <= limit :
+            left += 1
+        right -= 1
+        answer+=1
     return answer
