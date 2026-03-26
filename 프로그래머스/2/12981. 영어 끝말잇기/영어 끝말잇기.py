8dc0fa571f98db6157a7d40238bf616248a53032
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-26 17:28:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.15 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,21 +1,25 @@
 def solution(n, words):
     answer = []
-    arr = [words[0]]
-    num = 1
-    r = 1 
+    length=len(words)
+    prior = words[0]
+    number = 1
+    wordList = []
+    wordList.append(prior)
     
-    for i in range (1, len(words)) :
-        if words[i] not in arr and words[i][0] == words[i-1][len(words[i-1])-1] :
-            arr.append(words[i])
-            # print(arr)
-        elif (words[i] in arr or words[i][0] != words[i-1][len(words[i-1])-1]):
-            num = (i%n) + 1
-            r = i // n + 1
-            answer.append(num)
-            answer.append(r)
-            return answer
-        
-    return [0,0]
-        
-        
-   
+    for i in range(1, length) :
+        if prior[-1] == words[i][0] and words[i] not in wordList :
+            wordList.append(words[i])
+            number+=1
+            prior = words[i]
+        elif prior[-1] != words[i][0] or words[i] in wordList :
+            # number+=1
+            break
+            
+    if number == len(words) :
+        return [0,0]
+    number += 1
+    if number % n == 0 :
+        return [n, number//n]
+    else :
+        return [number%n, number //n + 1]
+    return answer
