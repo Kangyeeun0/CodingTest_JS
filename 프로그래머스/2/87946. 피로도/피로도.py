a85a51145731db17f02ec40bb71966e1b2900953
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-03-29 12:07:21 +0900

    [level 2] Title: 피로도, Time: 58.81 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,20 @@
 def solution(k, dungeons):
     visited = [False] * len(dungeons)
     
-    def dfs(k):
+    def dfs(k) :
         max_count = 0
         
-        for i in range(len(dungeons)):
+        for i in range(len(dungeons)) :
             need, use = dungeons[i]
             
-            if not visited[i] and k >= need:
+            if not visited[i] and k>=need :
                 visited[i] = True
-                count = 1 + dfs(k - use)
-                max_count = max(max_count, count)
+                count = 1 + dfs(k-use)               
+                max_count=max(max_count, count)
                 visited[i] = False
         
         return max_count
+                
+                
     
     return dfs(k)
