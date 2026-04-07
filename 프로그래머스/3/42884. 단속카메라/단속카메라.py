1a415cfeddfd84d0e9b8195c84a97c5a480c3b21
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-04-07 20:30:30 +0900

    [level 3] Title: 단속카메라, Time: 0.85 ms, Memory: 9.49 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,15 @@
+def solution(routes):
+    answer = 1
+    routes.sort(key = lambda x: x[1])
+    check = routes[0][1]
+    
+    for route in routes :
+        if route[0] <= check :
+            continue
+        else :
+            check = route[1]
+            answer+= 1
+        
+        
+        
+    return answer
