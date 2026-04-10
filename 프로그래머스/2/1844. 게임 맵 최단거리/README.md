86ca3c402a0b95b319557c92e95fead1797da02d
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-04-10 14:04:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.78 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 14.52 ms
+메모리: 9.48 MB, 시간: 16.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 00:43:06
+2026년 04월 10일 14:04:31
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,32 @@
 from collections import deque
-
 def solution(maps):
     answer = 0
-    n = len(maps)
-    m = len(maps[0])
+    visited = [[False] * len(maps[0]) for i in range(len(maps))]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
     
-    dx=[1,-1,0,0]
-    dy=[0,0,1,-1]
-    q= deque()
-    q.append((0,0))
+    q = deque()
+    q.append((0, 0, 1))
+    visited[0][0] = True
     
     while q :
-        x, y = q.popleft()
+        x,y, cnt = q.popleft()
+        
+        if x == len(maps)-1 and y == len(maps[0])-1 :
+            break
         
         for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                q.append((nx,ny))
+            if 0<=nx<len(maps) and 0<=ny<len(maps[0]) :
+                if not visited[nx][ny] and maps[nx][ny] == 1 : 
+                    visited[nx][ny] = True
+                    maps[nx][ny] = cnt+1
+                    q.append((nx,ny, cnt+1))
     
     if maps[-1][-1] == 1 :
         return -1
+                             
+                             
+                             
     return maps[-1][-1]
