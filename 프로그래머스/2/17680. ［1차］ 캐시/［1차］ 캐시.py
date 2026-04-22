3ae0a9f6063806ff21f0377bb30b2b8d9d2b32a7
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-04-22 14:43:40 +0900

    [level 2] Title: [1차] 캐시, Time: 77.41 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,23 +1,26 @@
 from collections import deque
 def solution(cacheSize, cities):
     answer = 0
-    queue = deque()
+    cache = deque()
     
-    if cacheSize == 0 :
+    if cacheSize <1 :
         return 5*len(cities)
     
-
-    for j in range(0, len(cities)) :
-        city = cities[j].upper()
-        if city in queue :
-            queue.remove(city)
-            answer+=1
-            queue.append(city)
-
+    for i in range(len(cities)) :
+        city = cities[i].upper()
+        if city in cache :
+                cache.remove(city)
+                cache.append(city)
+                answer+=1
         else :
-            if len(queue)>=cacheSize :
-                queue.popleft()
-            queue.append(city)
-            answer+=5
-                
+            if len(cache) < cacheSize :
+                cache.append(city)
+                answer+=5
+            
+            else :
+                cache.popleft()
+                cache.append(city)
+                answer+=5
+                    
+
     return answer
