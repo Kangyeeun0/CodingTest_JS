ba29cad7481abc53f392b072f6d3e4b8115b082b
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-04-22 14:25:22 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,21 @@
+import math
+from collections import deque
 def solution(progresses, speeds):
     answer = []
-    days=[]
+    q = deque()
+    n = len(speeds)
     
-    for i in range(len(progresses)) :
-        day = (100 - progresses[i] + speeds[i] - 1) // speeds[i]
-        days.append(day)
+    for i in range(n) :
+        remain_day = math.ceil((100-progresses[i])/speeds[i])
+        q.append(remain_day) 
         
-    count=1
-    current=days[0]
-    
-    for j in range(1, len(days)) :
-        if current < days[j] :
-            answer.append(count)
-            count=1
-            current=days[j]
-        else :
-            count+=1
-    answer.append(count)
-    
+    while q :
+        day = q.popleft()
+        total_day = 1
+        i=0
+        while q and day >=q[i] :
+            q.popleft()
+            total_day+=1
+        answer.append(total_day)
+        
     return answer
