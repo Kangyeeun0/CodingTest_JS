c61a3fd26508c8931cc890f2bd3cbf0b58bfefd4
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-04-23 00:05:09 +0900

    [level 2] Title: 롤케이크 자르기, Time: 482.51 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,20 +1,21 @@
+from collections import Counter
 def solution(topping):
     answer = 0
-    
-    right = {}
-    for t in topping:
-        right[t] = right.get(t, 0) + 1
-    
+    dic = dict()
     left = set()
+    right = len(set(topping))
     
-    for t in topping:
+    for t in topping :
+        dic[t] = dic.get(t, 0) + 1
+    for i in range(len(topping)) :
+        t = topping[i]
         left.add(t)
-        right[t] -= 1
+        dic[t] -= 1
+        if dic[t] == 0 :
+            right -= 1
+        if len(left) == right :
+            answer+=1
+
         
-        if right[t] == 0:
-            del right[t]
         
-        if len(left) == len(right):
-            answer += 1
-    
     return answer
