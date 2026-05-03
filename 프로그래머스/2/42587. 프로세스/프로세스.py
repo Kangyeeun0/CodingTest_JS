5fa82125fb790fab673bf9bae29c764b899161c4
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-05-03 14:43:15 +0900

    [level 2] Title: 프로세스, Time: 0.11 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,24 +1,26 @@
 from collections import deque
 def solution(priorities, location):
     answer = 1
-    q = deque()
+    waiting_q = deque()
+    sort_priorities = sorted(priorities, reverse = True)
+    
     for i in range(len(priorities)) :
-        q.append([i, priorities[i]])
-    # print(q)
-    priorities.sort(reverse=True)
+        waiting_q.append([i,priorities[i]])
+    # print(waiting_q)
     i=0
-    while q :
-        index, priority = q.popleft()
-        if priority == priorities[i] :
-            if index == location :
+    
+    while waiting_q :
+        l, p = waiting_q.popleft()
+        # print(l,p)
+        
+        if p == sort_priorities[i] :
+            if l == location :
                 return answer
             else :
                 answer+=1
-            i+=1
+                i+=1
         else :
-            q.append([index, priority])
+            waiting_q.append([l,p])
             
-            
-        
         
     return answer
