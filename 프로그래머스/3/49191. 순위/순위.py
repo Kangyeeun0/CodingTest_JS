cb11f18d035bd8a1de22c21ea75e3d9cb70f1df5
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-05-04 15:34:11 +0900

    [level 3] Title: 순위, Time: 8.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,31 +1,25 @@
+from collections import defaultdict
+
 def solution(n, results):
-    
-    win = [[False] * (n+1) for _ in range(n+1)]
-    
-    #직접 경기 결과 입력
-    for a,b in results :
-        win[a][b] = True
-        
-    for k in range(1, n+1) :
-        for i in range(1, n+1) :
-            for j in range(1, n+1) :
-                #i가 k를 이기고, k가 j를 이기면
-                #i는 j를 이김
-                if win[i][k] and win[k][j] :
-                    win [i][j] = True
-                    
     answer = 0
+    win = defaultdict(set)
+    lose = defaultdict(set)
+    
+    for result in results :
+        a, b = result
+        win[a].add(b)
+        lose[b].add(a)
     
     for i in range(1, n+1) :
-        known = 0 # i번 선수와 승패가 명확한 선수 수
-        
-        for j in range(1, n+1) :
-            if i != j:
-                #i가 j를 이기거나, j가 i를 이기면
-                if win[i][j] or win[j][i] :
-                    known += 1
-        
-        if known == n-1 :
-            answer += 1
+        for winner in lose[i] :
+            win[winner].update(win[i])
+        for loser in win[i] :
+            lose[loser].update(lose[i])
+            
+    # print(win, lose)
     
+    for i in range(1, n+1) :
+        if len(win[i]) + len(lose[i]) == n - 1 :
+            answer += 1
+            
     return answer
