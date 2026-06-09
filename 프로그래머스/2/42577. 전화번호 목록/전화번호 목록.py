3ae6086017366a847c91e4727cac0664fc5580cc
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-09 20:42:46 +0900

    [level 2] Title: 전화번호 목록, Time: 79.86 ms, Memory: 28.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,12 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
+    # print(phone_book)
     
     for i in range(len(phone_book)-1) :
         phone = phone_book[i]
+    
         if phone == phone_book[i+1][0:len(phone)] :
-            return False     
+            return False
             
     return answer
