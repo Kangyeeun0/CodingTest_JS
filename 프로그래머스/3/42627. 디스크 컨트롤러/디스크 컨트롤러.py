7b237a5dc967210a3ffb3294600ee759d254b62e
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-14 11:36:11 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.34 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,30 +1,30 @@
 import heapq
 def solution(jobs):
     answer = 0
-    jobs.sort(key=lambda x:x[0])
-    completed = 0
+    wait_q = []
+    heapq.heapify(wait_q)
+    heapq.heapify(jobs)
     current_time = 0
-    total_time = 0
-    job_index = 0
-    waiting_jobs = []
-    # heapq.heapify(waiting_jobs)
+    i=0
+    total_time = []
     
-    
-    while completed < len(jobs) :
-        while job_index < len(jobs) and jobs[job_index][0] <= current_time :
-                heapq.heappush(waiting_jobs,(jobs[job_index][1],jobs[job_index]))
-                job_index+=1
-        
-        if waiting_jobs :
-            priority, job = heapq.heappop(waiting_jobs)
-            current_time += job[1]
-            total_time += current_time - job[0]
-            completed+=1
-        else :
-            if job_index < len(jobs):
-                current_time = jobs[job_index][0]
-        
-        
-        
+    while jobs or wait_q :
+        while jobs and jobs[0][0] <= current_time :
+                job = heapq.heappop(jobs)
+                heapq.heappush(wait_q, [job[1], job[0]])
                 
-    return total_time //len(jobs)
+        if wait_q :
+            current_job = heapq.heappop(wait_q)
+            # print(current_job)
+            current_time += current_job[0]
+            # print(current_time)
+            total_time.append(current_time - current_job[1])
+        else :
+            current_time = jobs[0][0]
+            
+            
+    for i in range(len(total_time)) :
+        answer+=total_time[i]
+                
+
+    return answer // len(total_time)
