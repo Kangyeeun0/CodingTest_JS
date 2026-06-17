01980c8cd77c1ccc9ca60968f0f8da2eaf4c55ac
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-17 12:26:52 +0900

    [level 1] Title: 모의고사, Time: 1.20 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,20 @@
 def solution(answers):
-    answer =[]
-    people = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-    scores = []
+    answer = [0,0,0]
+    one = [1, 2, 3, 4, 5]
+    two = [2,1,2,3,2,4,2,5]
+    three = [3,3,1,1,2,2,4,4,5,5]
     
-    for i in range(3):
-        count = 0
-        for j in range(len(answers)) :
-            if people[i][j%len(people[i])] == answers[j] :
-                count +=1
-        scores.append(count)
-    max_score = max(scores)
-    for k in range(len(scores)) :
-        if max_score==scores[k] :
-            answer.append(k+1)
-    
-
-    return answer
+    for i in range(len(answers)) :
+        if answers[i] == one[i%len(one)] :
+            answer[0] += 1
+        if answers[i] == two[i%len(two)] :
+            answer[1] += 1
+        if answers[i] == three[i%len(three)] :
+            answer[2] += 1
+            
+    max_answer = max(answer)
+    result = []
+    for j in range(len(answer)) :
+        if max_answer == answer[j] :
+            result.append(j+1)
+    return result
