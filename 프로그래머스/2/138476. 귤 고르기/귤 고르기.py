696e9b74be5e3a044933e67f08a4b933dba994a5
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-18 21:50:38 +0900

    [level 2] Title: 귤 고르기, Time: 34.75 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,18 @@
 def solution(k, tangerine):
     answer = 0
-    d= {} # 딕셔너리 생성
+    dic = dict()
     
-    for t in tangerine :
-        d[t] = d.get(t, 0) + 1
+    for i in range(len(tangerine)) :
+        dic[tangerine[i]] = dic.get(tangerine[i], 0) + 1
+        
+    sorted_dic = dict(sorted(dic.items(), key=lambda x: x[1], reverse = True))
     
-    sorted_d = sorted(d.items(), key= lambda x: x[1], reverse = True)
-    i=0
-    total=0
-    while total < k :
-        total+=sorted_d[i][1]
-        i+=1
+    
+    for (key, item) in sorted_dic.items() :
+        k-= item
         answer+=1
+        
+        if k <=0 :
+            return answer
+    
     return answer
