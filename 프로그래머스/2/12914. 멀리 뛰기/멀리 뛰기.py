b579815e29fabd0bd9aa3700ede79b5caa6a319a
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-18 22:02:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,11 +1,8 @@
 def solution(n):
     answer = 0
-    dp = [0] * (n+1)
-    dp[0] = 1
-    dp[1] = 1
+    dp=[1] * (n+1)
     
-    for i in range(n-1) :
-        dp[i+2] = dp[i+1] + dp[i]
-    # print(dp)
-    answer = dp[-1]
-    return answer % 1234567
+    for i in range(2, n+1) :
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    return dp[-1] % 1234567
