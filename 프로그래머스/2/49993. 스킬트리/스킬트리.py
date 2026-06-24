479371ff73bd8378b75e5705d6ec8f1185bd649a
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-24 13:49:07 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,23 +1,20 @@
 def solution(skill, skill_trees):
     answer = 0
-    arr = list(skill)
-    
-    answerArr=[]
+    skill = list(skill)
     
     for i in range(len(skill_trees)) :
-        n=0
-        isSkill = True
-        for ch in skill_trees[i] :
-            if ch in skill and ch == skill[n] :
-                n+=1
-                isSkill = True
-            elif ch in skill and ch!=skill[n] :
-                n=0
-                isSkill = False
-                break
-        
-        if isSkill :
+        target = skill_trees[i]
+        k = 0
+        cnt = 0
+        for j in range(len(target)) :
+            if target[j] in skill :
+                if target[j] == skill[k] :
+                    k +=1
+                cnt+=1
+        if k == cnt :
             answer+=1
-            answerArr.append(skill_trees[i])
-        print(answerArr)    
+                    
+                
+                
+        
     return answer
