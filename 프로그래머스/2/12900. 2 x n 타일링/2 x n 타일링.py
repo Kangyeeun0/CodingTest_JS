a479c2967c7452ad65d3d623ae30e7a185e1dc8e
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-24 13:55:34 +0900

    [level 2] Title: 2 x n 타일링, Time: 6.93 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,17 +1,13 @@
 def solution(n):
+    MOD = 1000000007
     answer = 0
     dp = [0] * (n+1)
     
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
+    dp[1] = 1
     
-    dp[0]=1
-    dp[1]=1
-    
-    for i in range(2, n+1) :
-        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007
-    
-    # print(dp)
+    if n >= 2:
+        dp[2] = 2
+    for i in range(3, n+1) :
+        dp[i] = (dp[i-1] + dp[i-2])% MOD
+        
     return dp[-1] 
