66fec27e7da15a500b86e5eecc5207f43612b388
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-06-24 13:40:16 +0900

    [level 3] Title: 숫자 게임, Time: 54.37 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,18 +1,26 @@
+from collections import deque
 def solution(A, B):
     answer = 0
-    A.sort()
-    B.sort()
-    leng = len(A)
-    a_idx = 0
-    b_idx = 0
+    a_q = deque(sorted(A, reverse=True))
+    b_q = deque(sorted(B, reverse=True))
     
-    while a_idx < leng and b_idx < leng  :
-        if A[a_idx] < B[b_idx] :
-            answer+=1
-            a_idx +=1
-            b_idx +=1
-        else :
-            b_idx += 1
+    if a_q[-1] >= b_q[0] :
+        return 0
+    
+    while a_q :
+        if a_q and b_q :
+            if a_q[0] <b_q[0] :
+                a_q.popleft()
+                b_q.popleft()
+                answer+=1
+            else :
+                a_q.popleft()
+        
+    
+    
+    
+    
+    
     
     
     return answer
