0e00ed644cdbe662ab7cdf340e8d458c4e4cd242
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-15 00:07:20 +0900

    [level 3] Title: 입국심사, Time: 267.87 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,23 @@
 def solution(n, times):
-    answer = 0
-    # 이분탐색 범위 설정
     left = 1
-    right = max(times) * n # 최악의 경우 : 가장 느린 심사대에서 모두 처리
-    
-    answer = right
+    right = max(times) * n
+    answer =right
     
     while left <= right :
-        mid = (left+right) // 2
+        mid = (left+right)//2
         
-        #mid 시간 동안 심사 가능한 총 인원 계산
-        total = 0
+        count = 0
+        
         for time in times :
-            total += mid // time
-            
-        if total >= n : #mid 시간 안에 n명 이상 심사 가능
+            count += mid//time
+        
+        
+        if count >= n :
             answer = mid
-            right = mid -1
-        else :
-            left = mid + 1
-       
+            right = mid - 1
+        else:
+            left = mid+1
+    
+
+            
     return answer
