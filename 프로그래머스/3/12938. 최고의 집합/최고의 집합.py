6fbee40fc64c0e6e5edd3075cef86c9169cfa258
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-20 11:16:59 +0900

    [level 3] Title: 최고의 집합, Time: 0.50 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,4 +1,7 @@
-import heapq
+#heap은 오름차순 정렬과는 조금 다른 것
+#부모노드 <= 자식노드만 만족하면 됨
+#여기서 핵심은 heap의 가장 첫원소만 가장 최소값을 만족한다는 것.
+#가장 최솟값을 사용할 거면 heap이 맞고, 정렬이 필요하다면 리스트 정렬을 사용해야 함
 def solution(n, s):
     
     if n > s :
@@ -6,13 +9,9 @@ def solution(n, s):
     
     k = s // n
     r = s % n
-    answer = [k for _ in range(n)]
-    heapq.heapify(answer)
-    # print(answer)
-    
-    while r > 0 :
-        value = heapq.heappop(answer)
-        heapq.heappush(answer, value+1)
-        r-=1
-        
-    return sorted(answer)
+    answer = [k] * n
+
+    for i in range(r):
+        answer[-1-i] += 1
+
+    return answer
