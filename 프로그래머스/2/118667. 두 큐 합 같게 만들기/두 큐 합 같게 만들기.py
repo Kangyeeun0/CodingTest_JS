b1b4e24ad7cf3d2cec35290649601eee82ed37fb
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-21 21:10:39 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 52.70 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,36 +1,30 @@
 from collections import deque
 def solution(queue1, queue2):
     answer = 0
-    q1=deque(queue1)
+    q1 = deque(queue1)
     q2 = deque(queue2)
-    sum1 = sum(queue1)
-    sum2 = sum(queue2)
-    total = sum1+sum2
-    
-    if total%2 :
-        return -1
-    
-    target = total // 2
+    sum_1 = sum(q1)
+    sum_2 = sum(q2)
+    total = (sum_1 + sum_2) / 2
     i=0
-    while i<=len(queue1)*3 :
-        if sum1==target :
-            return answer
-        elif sum1 > target :
-            if not q1 :
-                return -1
-            k=q1.popleft()
-            q2.append(k)
-            sum1-=k
-            sum2+=k
-            answer+=1
-        elif sum1 < target :
-            if not q2 :
-                return -1
-            k=q2.popleft()
-            q1.append(k)
-            sum1+=k
-            sum2-=k
-            answer+=1
+    k = 4 * len(q1)
+    while i < k :
+        if sum_1 == sum_2 :
+            return i
+        elif sum_1 > sum_2 :
+            q= q1.popleft()
+            q2.append(q)
+            sum_1 -= q
+            sum_2 += q
+        elif sum_1 < sum_2 :
+            q = q2.popleft()
+            q1.append(q)
+            sum_1+=q
+            sum_2 -=q
         i+=1
-            
+
+    
+
+    
+    
     return -1
