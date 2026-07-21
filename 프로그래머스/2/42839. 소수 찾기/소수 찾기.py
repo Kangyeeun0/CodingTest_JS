a5693427e0b6d224f4c59e43a7e35ad0a101ba9c
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-21 20:47:40 +0900

    [level 2] Title: 소수 찾기, Time: 546.77 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,40 +1,42 @@
 def solution(numbers):
     answer = 0
-    arr_num = [n for n in numbers]
-    prime = set()
+    number_arr = [k for k in numbers]
+    visited = [False] * len(number_arr)
+    prime_set = set()
     
-    # 소수인지 판별 함수식 하나 필요
     def isPrime(num) :
-        result = True
         if num == 0 or num == 1 :
             return False
-        if num == 2 or num == 3 :
-            return True
-        for i in range(2, num//2+1) :
-            if num%i == 0 :
-                return False
+        elif num == 2 or num == 3 :
+            return True 
+        else :
+            for i in range(2, num//2+1) :
+                if num % i == 0 :
+                    return False
         return True
-            
-    visited = [False] * len(arr_num)
-    #숫자 위치 바꿔서 수 만드는 함수식 필요
-    def dfs(n, cnt, prime) :
-        num = n
         
-        if cnt > 0 :
-            # print(num)
-            if isPrime(int(num)) :
-                prime.add(int(num))
-                # print(prime)
-        
-        
-        for i in range(len(arr_num)) :
-            if not visited[i] :
-                num = n + arr_num[i]
-                # print(num)
-                visited[i] = True
-                dfs(num, cnt+1, prime)
-                visited[i] = False
-            
-    dfs("", 0, prime)
     
-    return len(prime)
+    def generateNum(num) :
+        nonlocal answer
+        
+        if isPrime(int(num)) and int(num) not in prime_set :
+            answer+=1
+            prime_set.add(int(num))
+        
+        
+        for i in range(len(number_arr)) :
+            if not visited[i] :
+                visited[i] = True
+                generateNum(num+number_arr[i])
+                visited[i] = False
+                
+
+    for i in range(len(number_arr)) :
+        if not visited[i] :
+            visited[i] = True
+            generateNum(number_arr[i])
+            visited[i] = False            
+            
+   
+
+    return answer
