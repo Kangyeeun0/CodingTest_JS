97154b3ba0edd095b1dcdcf19ef9ae1cb87de2ba
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-22 23:59:38 +0900

    [level 3] Title: 스티커 모으기(2), Time: 26.40 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,28 +1,21 @@
+#dp 문제
 def solution(sticker):
     answer = 0
-    n = len(sticker)
     
-    # 예외 처리
-    if n == 1:
+    if len(sticker) == 1 :
         return sticker[0]
-    if n == 2:
-        return max(sticker[0], sticker[1])
     
-    # 경우 1: 첫 번째 스티커를 선택할 수 있는 경우 (마지막 제외)
-    # 인덱스 0 ~ n-2까지 고려
-    dp1 = [0] * (n - 1)
+    dp1 = [0] * (len(sticker)-1)
+    dp2 = [0] * (len(sticker))
     dp1[0] = sticker[0]
-    dp1[1] = max(sticker[0], sticker[1])
-    
-    for i in range(2, n-1) :
-        dp1[i] = max(dp1[i-1],dp1[i-2] + sticker[i])
-        
-    dp2 = [0] * (n-1)
-    dp2[0] = sticker[1]
-    dp2[1] = max(sticker[1], sticker[2])
-    
-    for i in range(2, n-1) :
-        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i+1])
-   
+    dp2[1] = sticker[1]
 
-    return max(dp1[-1], dp2[-1])
+    
+    for i in range(1, len(sticker)-1) :
+        dp1[i] = max(dp1[i-2]+sticker[i], dp1[i-1])
+        
+    for j in range(2, len(sticker)) :
+        dp2[j] = max(dp2[j-2]+sticker[j], dp2[j-1])
+    
+
+    return max(dp1[-1],dp2[-1])
