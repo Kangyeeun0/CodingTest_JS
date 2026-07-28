a5c4eab7b92a621d0e58eb5687dddd715cc46650
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-07-28 12:54:37 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.83 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,37 +1,53 @@
+#음악 제목, 재생 시작되고 끝난 시각, 악보
+#조건 일치하는 곡이 여러 개면 라디오에서 재생된 시간이 제일 긴 음악 반환
 def solution(m, musicinfos):
-    answer = []
-    songs = []
-    titles=[]
+    answer = ''
+    m = list(m)
+    arr = []
     
-    def change_code(melody) :
-        melody = melody.replace("C#", 'c')
-        melody = melody.replace("D#", 'd')
-        melody = melody.replace("F#", 'f')
-        melody = melody.replace("G#", 'g')
-        melody = melody.replace("A#", 'a')
-        return melody
-              
+    def changeMin(time) :
+        hour, minute = time.split(":")
         
-    for i in range(len(musicinfos)) :
-        start, end, title, song = musicinfos[i].split(',')
-        start_h, start_m = map(int, start.split(':'))
-        end_h, end_m = map(int, end.split(':'))
-        playtime = (end_h * 60 + end_m) - (start_h * 60 + start_m)
-        song = change_code(song)
-        l = (playtime // len(song)) + 1
-        song = song * l
-        songs.append([song[0:playtime],playtime])
-        titles.append(title)
+        return int(hour) * 60 + int(minute)
+    
+    def changeShop(melodie) :
+        m_arr =[]
+        for i in range(len(melodie)) :
+            if melodie[i] == '#' :
+                m_arr[-1] = m_arr[-1].lower()
+            else :
+                m_arr.append(melodie[i])    
         
-    change_m = change_code(m)
-    # print(songs, change_m)
-    for j in range(len(songs)) :
-        melody, playtime = songs[j]
-        if change_m in melody :
-            answer.append([titles[j], playtime])
         
-    if answer :
-        answer.sort(key= lambda x : x[1], reverse = True)
-        print(answer)
-        return answer[0][0]
+        return "".join(m_arr)
+    
+
+    m = changeShop(m)
+    i=1
+    for musicinfo in musicinfos :
+        start, end, name, melodie = musicinfo.split(",")
+        total_time = changeMin(end)-changeMin(start)
+        # print(total_time)
+        melodie=list(melodie)
+
+        melodie = changeShop(melodie)
+        
+        if len(melodie) > total_time :
+            melodie = melodie[0:total_time]
+        else :
+            melodie = melodie*(total_time//len(melodie)+1)
+            melodie = melodie[0:total_time]
+        
+        if m in melodie :
+            arr.append([melodie,i, name])
+        i+=1
+    
+    arr.sort(key = lambda x:(-len(x[0]), x[1]))
+    
+    if arr :
+        return arr[0][2]
+            
+            
+            
+        
     return "(None)"
