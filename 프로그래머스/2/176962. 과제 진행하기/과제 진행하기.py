77ec2b104305d2cfa665a9f31ed4a5a661c4f167
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-08-24 00:07:50 +0900

    [level 2] Title: 과제 진행하기, Time: 0.53 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,53 +1,64 @@
 from collections import deque
-
 def solution(plans):
     answer = []
     waiting_works = deque()
-
-    # 시간 -> 분
-    def change_time(t):
-        h, m = map(int, t.split(":"))
-        return h * 60 + m
-
+    
+    def changeTime(text) :
+        hour, minute = text.split(":")
+        
+        return int(hour)*60+int(minute)
+    
     # 전처리
-    for plan in plans:
-        plan[1] = change_time(plan[1])
+    for plan in plans :
+        plan[1] = changeTime(plan[1])
         plan[2] = int(plan[2])
-
-    # 시작 시간순 정렬
-    plans.sort(key=lambda x: x[1])
-
-    for i in range(len(plans) - 1):
-        subject, start, play = plans[i]
-        next_start = plans[i + 1][1]
-
-        # 다음 과제 시작 전까지 남는 시간
-        remain_time = next_start - start
-
-        if play <= remain_time:
-            answer.append(subject)
-            remain_time -= play
-
-            # 남는 시간 동안 대기 과제 수행
-            while waiting_works and remain_time > 0:
-                w_subject, w_play = waiting_works.pop()
-
-                if w_play <= remain_time:
-                    answer.append(w_subject)
-                    remain_time -= w_play
-                else:
-                    waiting_works.append([w_subject, w_play - remain_time])
-                    break
-
-        else:
-            # 끝내지 못하면 남은 시간 저장
-            waiting_works.append([subject, play - remain_time])
-
-    # 마지막 과제는 무조건 완료
-    answer.append(plans[-1][0])
-
-    # 대기 과제 역순(LIFO) 처리
-    while waiting_works:
-        answer.append(waiting_works.pop()[0])
-
+        
+    plans.sort(key=lambda x:x[1])
+    # print(plans)
+    
+    current_time = plans[0][1]
+    for i in range(len(plans)):
+        
+        if i+1 < len(plans) :
+            if current_time + plans[i][2] <= plans[i+1][1] :
+                current_time += plans[i][2]
+                answer.append(plans[i][0])
+                possible_time = plans[i+1][1] - current_time
+                
+                while waiting_works and possible_time > 0 :
+                    work = waiting_works.pop()
+                    if work[2] <= possible_time :
+                        current_time += work[2]
+                        answer.append(work[0])
+                        possible_time -= work[2]
+                    else :
+                        remain = work[2] - possible_time
+                        current_time+=possible_time
+                        work[2] = remain
+                        waiting_works.append(work)
+                        possible_time = 0
+                        
+                # 밀린 과제까지 다 했는데도 시간이 남으면
+                # 현재 시간 다음 과제 시작 시간으로 설정
+                current_time = plans[i+1][1]
+                
+            else :
+                possible_time = plans[i+1][1] - current_time
+                current_time += possible_time
+                remain_time = plans[i][2] - possible_time
+                plans[i][2] = remain_time
+                waiting_works.append(plans[i])
+                
+        else :
+            answer.append(plans[i][0])
+            current_time += plans[i][2]
+            
+            
+    while waiting_works :
+        work = waiting_works.pop()
+        answer.append(work[0])
+            
+        
+        
+    
     return answer
