b762cae5fd4f4a578254f28afc933c888768a973
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-04 00:03:33 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,22 +2,20 @@ import math
 from collections import deque
 def solution(progresses, speeds):
     answer = []
-    rest = deque()
-    
+    rest_arr = deque([])
     for i in range(len(progresses)) :
-        res_day = math.ceil((100-progresses[i])/speeds[i])
-        rest.append(res_day)
-    # print(rest)
-    
-    cnt = 0
-    while rest :
-        first = rest.popleft()
-        cnt +=1
-        while rest and first>=rest[0] :
-            rest.popleft()
-            cnt+=1
-
-        answer.append(cnt)
-        cnt=0
+        rest = 100 - progresses[i]
+        rest_day = math.ceil(rest / speeds[i])
+        rest_arr.append(rest_day)
+        
+    while rest_arr :
+        task = rest_arr.popleft()
+        sum_task = 1
+        
+        while rest_arr and task >= rest_arr[0] :
+            rest_arr.popleft()
+            sum_task +=1
+            
+        answer.append(sum_task)
         
     return answer
