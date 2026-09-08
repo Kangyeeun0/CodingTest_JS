091d73c5e32b5c5207d64cf49fbe38d5b995492b
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-08 19:02:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,15 +1,16 @@
 def solution(nums):
     answer = 0
-    N = len(nums)
-    n = N // 2
-    dic = dict()
+    cnt = len(nums) / 2
+    dic = {}
     
     for num in nums :
         dic[num] = dic.get(num, 0) + 1
-    
-    if n <= len(dic) :
-        return n
+        
+    if cnt <= len(dic) :
+        return cnt
     else :
         return len(dic)
         
+    
+    
     return answer
