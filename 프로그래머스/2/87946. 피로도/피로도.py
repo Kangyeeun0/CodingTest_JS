2da2a8e4d07c42caeee390fd83498635608f21a1
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-09 14:10:54 +0900

    [level 2] Title: 피로도, Time: 17.25 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,26 @@
 def solution(k, dungeons):
-    answer = -1
+    answer = 0
     visited = [False] * len(dungeons)
     
-    def dfs(power, cnt) :
+    def dfs(k, num) :
         nonlocal answer
-        answer = max(answer, cnt)
+        answer = max(num, answer)
+        
+        if num == len(dungeons) :
+            return
         
         for i in range(len(dungeons)) :
-            if power >= dungeons[i][0] and not visited[i]:
-                power -= dungeons[i][1]
+            if not visited[i] and k >= dungeons[i][0]:
                 visited[i] = True
-                dfs(power, cnt+1)
-                ## 체력 감소하고 dfs 이후 돌아올 때 다시 체력 더해줘야 함
-                power+=dungeons[i][1]
+                dfs(k-dungeons[i][1], num+1)
                 visited[i] = False
                 
-        
-    dfs(k,0)
+        return 
     
+    for i in range(len(dungeons)) :
+        if k >= dungeons[i][0] :
+            visited[i] = True
+            dfs(k-dungeons[i][1], 1)
+            visited[i] = False
+                
     return answer
