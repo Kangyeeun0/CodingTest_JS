489cfbb47cbaecd3c5a1d8d416c0ea625ed61c0d
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-09 23:56:29 +0900

    [level 2] Title: 구명보트, Time: 8.16 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 11.8 MB, 시간: 7.58 ms
+메모리: 11.8 MB, 시간: 8.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 18일 17:36:16
+2026년 09월 09일 23:56:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,24 +1,20 @@
 def solution(people, limit):
     answer = 0
-    people.sort()
     left = 0
-    right = len(people) -1
+    right = len(people)-1
+    people.sort()
+    people_cnt = 0
     
     while left <= right :
-        tube = people[right]
-        right -=1
         
-        if tube+people[left] <= limit :
-            tube += people[left]
+        if people[left] + people[right] <=limit :
             left+=1
+            right-=1
+            answer+=1
+        else :
+            right -= 1
+            answer+=1
             
-        answer+=1
-            
-    
+        
     
     return answer
-
-
-
-
-
