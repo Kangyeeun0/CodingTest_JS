978506074d059b64d78d10d950bf21eb7ec043a8
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-10 12:54:09 +0900

    [level 3] Title: 단어 변환, Time: 1.57 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,42 +1,41 @@
-# 너비우선탐색인 거 같음
+from collections import deque
+def diffCount(word1,word2) :
+    cnt = 0
+    for i in range(len(word1)) :
+        if word1[i] != word2[i] :
+            cnt +=1
+        if cnt > 1 :
+            return False
+    
+    if cnt == 1:
+        return True
+    else :
+        return False    
+
 def solution(begin, target, words):
-    answer = len(words)
-    isSame = [False] * len(words)
+    answer = 0
+    q = deque()
+    visited = [False] * len(words)
     
     if target not in words :
         return 0
     
-    def cntDiff(word1, word2) :
-        cnt = 0
-        for i in range(len(word1)) :
-            if word1[i] != word2[i] :
-                cnt+=1
-        if cnt == 1:
-            return True
-        else :
-            return False
-        
-    def bfs(word, count) :
-        nonlocal answer
-        
+    q.append((begin, 0))
+    
+    while q :
+        word, cnt = q.popleft()
         
         if word == target :
-            answer = min(answer, count)
-            return answer
+            return cnt
         
         for i in range(len(words)) :
-            if not isSame[i] and cntDiff(word, words[i]) :
-                isSame[i] = True
-                bfs(words[i], count+1)
-                isSame[i] = False
-                
+            if not visited[i] :
+                if diffCount(word, words[i]) :
+                    q.append((words[i], cnt+1))
             
         
-        
-    for i in range(len(words)) :
-        if cntDiff(begin, words[i]) :
-            isSame[i] = True
-            bfs(words[i], 1)
-            isSame[i] = False
+    
+    
+    
     
     return answer
