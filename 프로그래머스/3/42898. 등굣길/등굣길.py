45845045faae073b30ba694d31d0f2b190c4c740
강예은 <102957509+Kangyeeun0@users.noreply.github.com>
2026-09-10 12:07:03 +0900

    [level 3] Title: 등굣길, Time: 2.36 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,23 +1,22 @@
 def solution(m, n, puddles):
     answer = 0
-    dp = [[0] * m for _ in range(n)]
-    puddles = set(map(tuple, puddles))
+    dp = [[0] * (m+1) for _ in range(n+1)]
     
-    dp[0][0] = 1
+    # print(dp)
+    dp[1][1] = 1
     
-    for i in range(n) :
-        for j in range(m) :
-            if i == 0 and j == 0 :
+    for i in range(1,n+1) :
+        for j in range(1, m+1) :
+            if i == 1 and j == 1 :
                 continue
-            if (j+1,i+1) in puddles :
+            if [j,i] in puddles :
                 dp[i][j] = 0
             else :
-                if 0<=i-1<n and 0<=j-1<m :
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-                elif 0<=i-1<n :
-                    dp[i][j] = dp[i-1][j]
-                elif 0<=j-1<m :
-                    dp[i][j] = dp[i][j-1]
-                
-    # print(dp)
-    return dp[-1][-1] % 1000000007
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            
+            
+            
+    # print(dp)        
+    
+    
+    return dp[-1][-1] %1000000007
